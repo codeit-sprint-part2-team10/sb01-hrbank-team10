7bf9e1e92fcfdd835095410fa094f72d1f563d08
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-03-13 18:45:01 +0900

    init

---
+++ src/main/java/com/sprint/example/sb01part2hrbankteam10/Sb01Part2HrbankTeam10Application.java
@@ -0,0 +1,13 @@
+package com.sprint.example.sb01part2hrbankteam10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb01Part2HrbankTeam10Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Sb01Part2HrbankTeam10Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/sprint/example/sb01part2hrbankteam10/Sb01Part2HrbankTeam10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.example.sb01part2hrbankteam10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb01Part2HrbankTeam10ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

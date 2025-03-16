c4cb021d1047ef1f802a2f6f0d23a47ef55f7dc5
Jeong Yeonkyung <edaily0129@gmail.com>
2025-03-17 00:29:25 +0900

    OPS-79 feat: 엔티티 생성 및 JPA 적용

---
+++ src/main/java/com/sprint/example/sb01part2hrbankteam10/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.sprint.example.sb01part2hrbankteam10.repository;
+
+import com.sprint.example.sb01part2hrbankteam10.entity.Department;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
+  Optional<Object> findByName(String name);
+}

64bd6c3ca640e1bb556fac9cdbfcac12641cb231
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-03-17 09:24:59 +0900

    OPS-82 fix: 어노테이션-수정

--- src/main/java/com/sprint/example/sb01part2hrbankteam10/repository/FileRepository.java
+++ src/main/java/com/sprint/example/sb01part2hrbankteam10/repository/FileRepository.java
@@ -1,10 +1,10 @@
 package com.sprint.example.sb01part2hrbankteam10.repository;
 
 import com.sprint.example.sb01part2hrbankteam10.entity.File;
-import org.springframework.context.annotation.ReflectiveScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@ReflectiveScan
+@Repository
 public interface FileRepository extends JpaRepository<File,Integer> {
 
 }

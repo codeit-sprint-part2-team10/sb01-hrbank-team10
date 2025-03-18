d5b559c538e383936b0ff08fa06d0e7bce85772a
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-03-18 17:33:24 +0900

    OPS-31 feat:Employee 조회 구현

--- src/main/java/com/sprint/example/sb01part2hrbankteam10/controller/EmployeeController.java
+++ src/main/java/com/sprint/example/sb01part2hrbankteam10/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +54,15 @@ public class EmployeeController {
         .body(RestApiResponse.success(
             HttpStatus.OK,
             employeeService.update(id, request, profile, IpUtil.getClientIp(httpServletRequest))
+        ));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<RestApiResponse<EmployeeDto>> getEmployee (@PathVariable Integer id) {
+    return ResponseEntity.ok()
+        .body(RestApiResponse.success(
+            HttpStatus.OK,
+            employeeService.getById(id)
         ));
   }
 }

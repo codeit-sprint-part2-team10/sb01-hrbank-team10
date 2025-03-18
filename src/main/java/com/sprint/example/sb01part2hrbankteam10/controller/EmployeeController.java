716712ca0dab9a0c65b4566b24393935d14b7f23
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-03-18 10:38:38 +0900

    OPS-95 feat: client ip util 추가

--- src/main/java/com/sprint/example/sb01part2hrbankteam10/controller/EmployeeController.java
+++ src/main/java/com/sprint/example/sb01part2hrbankteam10/controller/EmployeeController.java
@@ -4,8 +4,11 @@ import com.sprint.example.sb01part2hrbankteam10.dto.EmployeeCreateRequest;
 import com.sprint.example.sb01part2hrbankteam10.dto.EmployeeDto;
 import com.sprint.example.sb01part2hrbankteam10.global.response.RestApiResponse;
 import com.sprint.example.sb01part2hrbankteam10.service.EmployeeService;
+import com.sprint.example.sb01part2hrbankteam10.util.IpUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
@@ -23,9 +27,13 @@ public class EmployeeController {
 
   @PostMapping
   public ResponseEntity<RestApiResponse<EmployeeDto>> createEmployee (
+      HttpServletRequest httpServletRequest,
       @Valid @RequestPart(name = "employee")EmployeeCreateRequest request,
       @RequestPart(name = "profile", required = false) MultipartFile profile
   ) {
+
+    log.info(IpUtil.getClientIp(httpServletRequest));
+
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(RestApiResponse.success(
             HttpStatus.CREATED,

090dd83b7d246c2fe7ba05bc9fb79d7367d2aa26
mingi96 <minnet1030@naver.com>
2025-03-19 16:03:21 +0900

    직원 정보 수정 이력 상세 조회 구현

--- src/main/java/com/sprint/example/sb01part2hrbankteam10/global/exception/errorcode/EmployeeHistoryErrorCode.java
+++ src/main/java/com/sprint/example/sb01part2hrbankteam10/global/exception/errorcode/EmployeeHistoryErrorCode.java
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum EmployeeHistoryErrorCode implements ErrorCode {
 
     EMPLOYEE_HISTORY_NOT_FOUND(HttpStatus.NOT_FOUND, "EMPLOYEE_HISTORY_001", "해당 직원 이력이 존재하지 않습니다."),
-    INVALID_SORT_FIELD(HttpStatus.BAD_REQUEST, "EMPLOYEE_HISTORY_002", "지원하지 않는 정렬 필드입니다.");
-
+    INVALID_SORT_FIELD(HttpStatus.BAD_REQUEST, "EMPLOYEE_HISTORY_002", "잘못된 요청입니다."),
+    INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "EMPLOYEE_HISTORY_003", "유효하지 않은 날짜 범위입니다.");
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;

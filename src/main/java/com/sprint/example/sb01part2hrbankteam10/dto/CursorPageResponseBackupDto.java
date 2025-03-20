fa22f1ceeb021587cdbb0345ab12a746c00c81be
Yeonkyung-Jeong <edaily0129@gmail.com>
2025-03-21 01:15:52 +0900

    refactor: backup 목록 조회 반환값 DTO로 수정

---
+++ src/main/java/com/sprint/example/sb01part2hrbankteam10/dto/CursorPageResponseBackupDto.java
@@ -0,0 +1,21 @@
+package com.sprint.example.sb01part2hrbankteam10.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CursorPageResponseBackupDto {
+    List<BackupDto> content;
+    String nextCursor;
+    Integer nextIdAfter;
+    Integer size;
+    Integer totalElements;
+    boolean hasNext;
+}
